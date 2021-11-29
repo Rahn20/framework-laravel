3c20ae284a904b7abcde18c34c1881c3fb850846
unknown <ranimhanna@hotmail.com>
2021-11-29 00:28:55 +0000

    routes

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Web Routes
+ *
+*/
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\YatzyController;
+
+// index page
+Route::view('/', 'home')->name('home');
+
+// Game21 routes
+Route::view('/game', 'game')->name('game');
+Route::post('/game', [ GameController::class, 'play' ])->name('play-game');
+Route::get('/game/destroy', [ GameController::class, 'destroyGame' ])->name('destroy-game');
+
+// yatzy routes
+Route::view('/yatzy', 'yatzy')->name('yatzy');
+Route::post('/yatzy', [ YatzyController::class, 'play' ])->name('play-yatzy');
+Route::get('/yatzy/destroy', [ YatzyController::class, 'destroyYatzy' ])->name('destroy-yatzy');
