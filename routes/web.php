28006bd5e45f7179c9a2e646a441256670fc5649
unknown <ranimhanna@hotmail.com>
2022-01-18 23:01:50 +0000

    Add 2 new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Web Routes
  *
@@ -8,6 +7,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\YatzyController;
+use App\Http\Controllers\HighscoreController;
 
 // index page
 Route::view('/', 'home')->name('home');
@@ -21,3 +21,9 @@ Route::get('/game/destroy', [ GameController::class, 'destroyGame' ])->name('des
 Route::view('/yatzy', 'yatzy')->name('yatzy');
 Route::post('/yatzy', [ YatzyController::class, 'play' ])->name('play-yatzy');
 Route::get('/yatzy/destroy', [ YatzyController::class, 'destroyYatzy' ])->name('destroy-yatzy');
+
+
+//highScore for Game21
+$scores = new HighscoreController();
+Route::view('/highscore', 'highscore', ['scores' => $scores->getAllScores()])->name('highscore');
+Route::get('/highscore/{id}', [ HighscoreController::class, 'deleteHighScoreById' ])->name('delete-one');
